a0187c39c8f3d44e4ce70082bb20ce1ca1846a78
henri2h <henri.carnot@hotmail.fr>
2020-12-14 23:24:15 +0100

    Added friends vue

---
+++ lib/screens/friendsVue.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FriendsVue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Friends")),
+      body: Container(child: Text("hello")),
+    );
+  }
+}
--- lib/screens/home/left_bar/widget.dart
+++ lib/screens/home/left_bar/widget.dart
@@ -1,72 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:minestrix/screens/chatsVue.dart';
+import 'package:minestrix/screens/friendsVue.dart';
 import 'package:minestrix/screens/smatrixRoomsVue.dart';
 
 class LeftBar extends StatelessWidget {
   const LeftBar({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          LeftBarButton(name: "Feed", icon: Icons.home),
-          LeftBarButton(name: "My account", icon: Icons.person),
-          LeftBarButton(name: "Friends", icon: Icons.people),
-          LeftBarButton(name: "Settings", icon: Icons.settings),
-          LeftBarButton(name: "Debug", icon: Icons.bug_report),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => SMatrixRoomsVue(),
-                    ),
-                  );
-                  print("push");
-                },
-                child: LeftBarButton(name: "Smatrix rooms", icon: Icons.lock)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => ChatsVue(),
-                    ),
-                  );
-                },
-                child: LeftBarButton(name: "Chat", icon: Icons.message)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(
-                onPressed: () {
-                  print("push");
-                },
-                child: LeftBarButton(name: "Logout", icon: Icons.logout)),
-          ),
-        ]);
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+        Widget>[
+      LeftBarButton(name: "Feed", icon: Icons.home, onPressed: () {}),
+      LeftBarButton(name: "My account", icon: Icons.person, onPressed: () {}),
+      LeftBarButton(name: "Friends", icon: Icons.people, onPressed: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => FriendsVue(),
+            ),
+          );
+      }),
+      LeftBarButton(name: "Settings", icon: Icons.settings, onPressed: () {}),
+      LeftBarButton(
+        name: "Debug",
+        icon: Icons.bug_report,
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => SMatrixRoomsVue(),
+            ),
+          );
+        },
+      ),
+      LeftBarButton(
+        name: "SMatrix rooms",
+        icon: Icons.lock,
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => SMatrixRoomsVue(),
+            ),
+          );
+        },
+      ),
+      LeftBarButton(
+        name: "Chat",
+        icon: Icons.message,
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => ChatsVue(),
+            ),
+          );
+        },
+      ),
+      LeftBarButton(
+        name: "Logout",
+        icon: Icons.logout,
+        onPressed: () {},
+      ),
+    ]);
   }
 }
 
 class LeftBarButton extends StatelessWidget {
-  const LeftBarButton({Key key, this.name, this.icon}) : super(key: key);
+  const LeftBarButton({Key key, this.name, this.icon, this.onPressed})
+      : super(key: key);
   final String name;
   final IconData icon;
+  final Function onPressed;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Icon(icon),
-          SizedBox(width: 5),
-          Text(name,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
-        ],
+      padding: const EdgeInsets.all(8),
+      child: RaisedButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Icon(icon),
+              SizedBox(width: 5),
+              Text(name,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+            ],
+          ),
+        ),
       ),
     );
   }
