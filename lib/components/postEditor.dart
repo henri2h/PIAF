374900961dc97d1838d6b5cec27c8038def5c92d
henri2h <henri.carnot@hotmail.fr>
2020-12-21 02:18:08 +0100

    removed annimation

--- lib/components/postEditor.dart
+++ lib/components/postEditor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class PostEditor extends StatefulWidget {
   @override
   _PostEditorState createState() => _PostEditorState();
@@ -6,29 +7,11 @@ class PostEditor extends StatefulWidget {
 
 class _PostEditorState extends State<PostEditor>
     with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text("Hello"),
-          TextField()
-        ],
-      )
-    );
+        child: Column(
+      children: [Text("Hello"), TextField()],
+    ));
   }
 }
