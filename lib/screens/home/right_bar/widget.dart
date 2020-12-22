45eac93b23de1fdf9c332f4b871c28b178186004
henri2h <henri.carnot@hotmail.fr>
2020-12-23 00:29:50 +0100

    Adding some other flexible

--- lib/screens/home/right_bar/widget.dart
+++ lib/screens/home/right_bar/widget.dart
@@ -32,44 +32,47 @@ class ContactView extends StatelessWidget {
   Widget build(BuildContext context) {
     final SClient client = Matrix.of(context).sclient;
 
-      return SizedBox(
-          child: Card(
-              child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
+    return SizedBox(
+        child: Card(
+            child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                                              child: Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             CircleAvatar(
-                              backgroundImage:
-                                  sroom.user == null || sroom.user.avatarUrl == null
-                                      ? null
-                                      : NetworkImage(
-                                          sroom.user.avatarUrl.getThumbnail(
-                                            client,
-                                            width: 64,
-                                            height: 64,
-                                          ),
-                                        ),
+                              backgroundImage: sroom.user == null ||
+                                      sroom.user.avatarUrl == null
+                                  ? null
+                                  : NetworkImage(
+                                      sroom.user.avatarUrl.getThumbnail(
+                                        client,
+                                        width: 64,
+                                        height: 64,
+                                      ),
+                                    ),
                             ),
                             Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(sroom.user.displayName,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    Text(sroom.user.id)
-                                  ]),
+                              child: Flexible(
+                                                              child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(sroom.user.displayName,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      Text(sroom.user.id, overflow: TextOverflow.ellipsis,)
+                                    ]),
+                              ),
                             ),
                           ],
                         ),
-                        if (sroom.room.encrypted) Icon(Icons.verified_user),
-                        if (!sroom.room.encrypted) Icon(Icons.no_encryption)
-                      ]))));
-   
+                      ),
+                      if (sroom.room.encrypted) Icon(Icons.verified_user),
+                      if (!sroom.room.encrypted) Icon(Icons.no_encryption)
+                    ]))));
   }
 }
