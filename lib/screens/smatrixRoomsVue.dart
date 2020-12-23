794fd4523a14a017522b340260570486e65abe3c
henri2h <henri.carnot@hotmail.fr>
2020-12-23 01:44:17 +0100

    moved to widget

--- lib/screens/smatrixRoomsVue.dart
+++ lib/screens/smatrixRoomsVue.dart
@@ -13,37 +13,39 @@ class _SMatrixRoomsVueState extends State<SMatrixRoomsVue>
   @override
   Widget build(BuildContext context) {
     final sclient = Matrix.of(context).sclient;
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("Hello"),
-          actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})]),
-      body: Container(
-        child: StreamBuilder(
-          stream: sclient.onSync.stream,
-          builder: (context, _) => ListView.builder(
-            itemCount: sclient.srooms.length,
-            itemBuilder: (BuildContext context, int i) => ListTile(
-              leading: CircleAvatar(
-                backgroundImage: sclient.srooms[i].room.avatar == null
-                    ? null
-                    : NetworkImage(
-                        sclient.srooms[i].room.avatar.getThumbnail(
-                          sclient,
-                          width: 64,
-                          height: 64,
-                        ),
-                      ),
-              ),
-              title: Text(sclient.srooms[i].room.displayname),
-              subtitle: Text(sclient.srooms[i].room.lastMessage),
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => ChatView(roomId: sclient.srooms[i].room.id),
+    return Container(
+      child: Column(
+        children: [
+          Flexible(
+            child: StreamBuilder(
+              stream: sclient.onSync.stream,
+              builder: (context, _) => ListView.builder(
+                itemCount: sclient.srooms.length,
+                itemBuilder: (BuildContext context, int i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: sclient.srooms[i].room.avatar == null
+                        ? null
+                        : NetworkImage(
+                            sclient.srooms[i].room.avatar.getThumbnail(
+                              sclient,
+                              width: 64,
+                              height: 64,
+                            ),
+                          ),
+                  ),
+                  title: Text(sclient.srooms[i].room.displayname),
+                  subtitle: Text(sclient.srooms[i].room.lastMessage),
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) =>
+                          ChatView(roomId: sclient.srooms[i].room.id),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
