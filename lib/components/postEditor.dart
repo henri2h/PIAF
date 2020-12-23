51b8e992d6952a3352310b4bf5a36cdeaf028311
henri2h <henri.carnot@hotmail.fr>
2020-12-23 01:52:29 +0100

    added markdown support for text editor

--- lib/components/postEditor.dart
+++ lib/components/postEditor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class PostEditor extends StatefulWidget {
   @override
@@ -7,11 +8,35 @@ class PostEditor extends StatefulWidget {
 
 class _PostEditorState extends State<PostEditor>
     with SingleTickerProviderStateMixin {
+  String postContent = "";
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Column(
-      children: [Text("Hello"), TextField()],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text("What's up ?",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            onChanged: (String text) {
+              setState(() {
+                postContent = text;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MarkdownBody(data:postContent),
+        )
+      ],
     ));
   }
 }
