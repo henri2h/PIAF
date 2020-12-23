a0c8611b31e45de335a5f4e2820168b10c4c71e1
henri2h <henri.carnot@hotmail.fr>
2020-12-23 01:24:26 +0100

    Moved chatsVue to widget

--- lib/screens/chatsVue.dart
+++ lib/screens/chatsVue.dart
@@ -13,37 +13,44 @@ class _ChatsVueState extends State<ChatsVue>
   @override
   Widget build(BuildContext context) {
     final client = Matrix.of(context).sclient;
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("Hello"),
-          actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})]),
-      body: Container(
-        child: StreamBuilder(
-          stream: client.onSync.stream,
-          builder: (context, _) => ListView.builder(
-            itemCount: client.rooms.length,
-            itemBuilder: (BuildContext context, int i) => ListTile(
-              leading: CircleAvatar(
-                backgroundImage: client.rooms[i].avatar == null
-                    ? null
-                    : NetworkImage(
-                        client.rooms[i].avatar.getThumbnail(
-                          client,
-                          width: 64,
-                          height: 64,
-                        ),
-                      ),
-              ),
-              title: Text(client.rooms[i].displayname),
-              subtitle: Text(client.rooms[i].lastMessage),
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => ChatView(roomId: client.rooms[i].id),
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text("MATRIX Chats",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          ),
+          Flexible(
+            child: StreamBuilder(
+              stream: client.onSync.stream,
+              builder: (context, _) => ListView.builder(
+                itemCount: client.rooms.length,
+                itemBuilder: (BuildContext context, int i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: client.rooms[i].avatar == null
+                        ? null
+                        : NetworkImage(
+                            client.rooms[i].avatar.getThumbnail(
+                              client,
+                              width: 64,
+                              height: 64,
+                            ),
+                          ),
+                  ),
+                  title: Text(client.rooms[i].displayname),
+                  subtitle: Text(client.rooms[i].lastMessage),
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => ChatView(roomId: client.rooms[i].id),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
