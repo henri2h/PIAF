211a3fd07eb7b1d10bf2360fc9bfe966510a0f7e
henri2h <henri.carnot@hotmail.fr>
2020-12-25 01:40:23 +0100

    added post logic

--- lib/components/postEditor.dart
+++ lib/components/postEditor.dart
@@ -1,5 +1,8 @@
+import 'package:famedlysdk/famedlysdk.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:minestrix/global/smatrix.dart';
+import 'package:minestrix/global/smatrixWidget.dart';
 
 class PostEditor extends StatefulWidget {
   @override
@@ -9,53 +12,63 @@ class PostEditor extends StatefulWidget {
 class _PostEditorState extends State<PostEditor>
     with SingleTickerProviderStateMixin {
   String postContent = "";
+
+  Future<void> sendPost(SClient sclient, String postContent,
+      {Event inReplyTo}) async {
+    await sclient.userRoom.room
+        .sendTextEvent(postContent, inReplyTo: inReplyTo);
+  }
+
   @override
   Widget build(BuildContext context) {
+    SClient sclient = Matrix.of(context).sclient;
     return Container(
-      padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text("What's up ?",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Post on your wall"),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            minLines: 3,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(), labelText: "Post content"),
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            onChanged: (String text) {
-              setState(() {
-                postContent = text;
-              });
-            },
-          ),
-        ),
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: FloatingActionButton.extended(
-                        icon: const Icon(Icons.send),
-                        label: Text('Send post'),
-                        onPressed: () {}),
-         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Post preview", style: TextStyle(fontSize: 20)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: MarkdownBody(data: postContent),
-        )
-      ],
-    ));
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text("What's up ?",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Post on your wall"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                minLines: 3,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Post content"),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                onChanged: (String text) {
+                  setState(() {
+                    postContent = text;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FloatingActionButton.extended(
+                  icon: const Icon(Icons.send),
+                  label: Text('Send post'),
+                  onPressed: () {
+                    sendPost(sclient, postContent);
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Post preview", style: TextStyle(fontSize: 20)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MarkdownBody(data: postContent),
+            )
+          ],
+        ));
   }
 }
