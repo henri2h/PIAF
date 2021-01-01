2b99b9024b352c2ee555f24e8a83111adc88dedc
henri2h <henri.carnot@hotmail.fr>
2021-01-01 23:28:37 +0100

    enhanced chat vue ui

--- lib/screens/chatsVue.dart
+++ lib/screens/chatsVue.dart
@@ -1,9 +1,11 @@
 import 'package:famedlysdk/famedlysdk.dart';
 import 'package:flutter/material.dart';
+import 'package:minestrix/components/Theme.dart';
 import 'package:minestrix/components/minesTrix/MinesTrixUserImage.dart';
 import 'package:minestrix/components/pageTitle.dart';
 import 'package:minestrix/global/smatrixWidget.dart';
 import 'package:minestrix/screens/chatVue.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ChatsVue extends StatefulWidget {
   @override
@@ -21,12 +23,61 @@ class _ChatsVueState extends State<ChatsVue>
         builder: (context, _) => ListView.builder(
             itemCount: client.rooms.length + 1,
             itemBuilder: (BuildContext context, int i) {
-              if (i == 0) return PageTitle("MATRIX Chats");
+              if (i == 0)
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    PageTitle("MATRIX Chats"),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child:
+                          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+                    ),
+                  ],
+                );
               int pos = i - 1;
               return ListTile(
-                leading: MatrixUserImage(url: client.rooms[pos].avatar),
-                title: Text(client.rooms[pos].displayname),
-                subtitle: Text(client.rooms[pos].lastMessage),
+                contentPadding: EdgeInsets.all(8),
+                focusColor: Colors.grey,
+                hoverColor: Colors.grey,
+                enableFeedback: true,
+                leading: MatrixUserImage(
+                    url: client.rooms[pos].avatar, width: 50, height: 50),
+                title: Text(client.rooms[pos].displayname,
+                    style: TextStyle(fontWeight: FontWeight.w600)),
+                trailing: Column(
+                  children: [
+                    Text(
+                        timeago
+                            .format(client.rooms[pos].lastEvent.originServerTs),
+                        style: TextStyle(fontSize: 14, color: Colors.grey)),
+                    if (client.rooms[pos].notificationCount != 0)
+                      Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: Material(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            elevation: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: MinesTrixTheme.buttonGradient,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical:4, horizontal:8),
+                                child: Text(
+                                    client.rooms[pos].notificationCount
+                                        .toString(),
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )),
+                      )
+                  ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(client.rooms[pos].lastMessage, maxLines: 2),
+                ),
                 onTap: () => Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (_) => ChatView(roomId: client.rooms[pos].id),
