e8311944f921fdb9c0da5876bae1b9c2407ffd46
henri2h <henri.carnot@hotmail.fr>
2021-01-14 19:11:04 +0100

    feat : added encrytipon informations

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -6,7 +6,7 @@ import 'package:minestrix/global/smatrix.dart';
 class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SClient client = Matrix.of(context).sclient;
+    SClient sclient = Matrix.of(context).sclient;
     final TextEditingController _passphraseController = TextEditingController();
     return Padding(
         padding: const EdgeInsets.all(8.0),
@@ -20,15 +20,37 @@ class SettingsView extends StatelessWidget {
             MaterialButton(
                 child: Text("Get keys"),
                 onPressed: () async {
-                  await client.encryption.crossSigning
+                  await sclient.encryption.crossSigning
                       .selfSign(passphrase: _passphraseController.text);
                   _passphraseController.text = "";
                 }),
             MaterialButton(
                 child: Text("logout ?"),
                 onPressed: () async {
-                  await client.logout();
-                })
+                  await sclient.logout();
+                }),
+                H2Title("Encryption"),
+                sclient.encryptionEnabled ? Column(
+                  children: [
+                    Text("Encrytpion enabled ✅"),
+
+                    Text("Encryption.enabled : " + sclient.encryption.enabled.toString()),
+                    Text("Cross signing enabled : " + sclient.encryption.crossSigning.enabled.toString()),
+                    Text("Unknown session : " + sclient.isUnknownSession.toString()),
+
+                    if(sclient.isUnknownSession == false) Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.check, size:50),
+                          SizedBox(width:10),
+                          Flexible(child: Text("Verified session, you're good to go !!", style:TextStyle(fontSize: 20)))
+                        ],
+                      ),
+                    )
+                  ],
+                ) : Text("Encrytpion disabled ❌"),
+                
           ],
         ));
   }
