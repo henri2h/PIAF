65715b83b8909b9684bfae98eb28ea8a6726616d
henri2h <henri.carnot@hotmail.fr>
2021-01-17 15:08:33 +0100

    feat : added a way to create an account

--- lib/screens/createMinesTrixAccount.dart
+++ lib/screens/createMinesTrixAccount.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:minestrix/components/minesTrix/MinesTrixButton.dart';
 import 'package:minestrix/components/minesTrix/MinesTrixTitle.dart';
+import 'package:minestrix/global/smatrix.dart';
+import 'package:minestrix/global/smatrixWidget.dart';
 
-class MinesTrixAccountCreation extends StatelessWidget {
+class MinesTrixAccountCreation extends StatefulWidget {
+  @override
+  _MinesTrixAccountCreationState createState() =>
+      _MinesTrixAccountCreationState();
+}
+
+class _MinesTrixAccountCreationState extends State<MinesTrixAccountCreation> {
+  bool running = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            body: ListView(
-      children: [
-        H1Title("Account creation"),
-        Text("hello"),
-        FlatButton(onPressed: () {}, child: Text("create account"))
-      ],
+            body: Padding(
+      padding: const EdgeInsets.all(30),
+      child: ListView(
+        children: [
+          H1Title("Ready to take part in the adventure ?"),
+          SizedBox(height: 40),
+          Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: running
+                  ? null
+                  : MinesTrixButton(
+                      onPressed: () async {
+                        setState(() {
+                          running = true;
+                        });
+                        SClient sclient = Matrix.of(context).sclient;
+
+                        await sclient.createSMatrixRoom();
+                      },
+                      label: "Go",
+                      icon: Icons.send)),
+          if (running) LinearProgressIndicator(),
+        ],
+      ),
     )));
   }
 }
