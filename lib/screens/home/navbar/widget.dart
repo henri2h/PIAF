7c9d02283bbfadbac48d3ba088c9a2a733459a7b
henri2h <henri.carnot@hotmail.fr>
2021-01-27 19:36:34 +0100

    feat : added button

--- lib/screens/home/navbar/widget.dart
+++ lib/screens/home/navbar/widget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:minestrix/global/helpers/NavigationHelper.dart';
+import 'package:minestrix/global/smatrix.dart';
+import 'package:minestrix/global/smatrixWidget.dart';
 
 class NavBar extends StatelessWidget {
   const NavBar({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    SClient sclient = Matrix.of(context).sclient;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(children: [
@@ -12,30 +17,42 @@ class NavBar extends StatelessWidget {
           child: Text("Mines'Trix",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
         ),
-        NavBarButton(name: "Feed", icon: Icons.home),
-        NavBarButton(name: "My Acount", icon: Icons.person),
-        NavBarButton(name: "Friends", icon: Icons.people)
+        NavBarButton(name: "Feed", icon: Icons.home, onPressed: () {}),
+        NavBarButton(
+            name: "My Acount",
+            icon: Icons.person,
+            onPressed: () {
+              NavigationHelper.navigateToUserFeed(
+                  context, sclient.userRoom.user);
+            }),
+        NavBarButton(name: "Friends", icon: Icons.people, onPressed: () {})
       ]),
     );
   }
 }
 
 class NavBarButton extends StatelessWidget {
-  const NavBarButton({Key key, this.name, this.icon}) : super(key: key);
+  const NavBarButton({Key key, this.name, this.icon, @required this.onPressed})
+      : super(key: key);
   final String name;
   final IconData icon;
+  final Function onPressed;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Icon(icon),
-          SizedBox(width: 5),
-          Text(name,
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400)),
-        ],
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(primary: Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Icon(icon),
+            SizedBox(width: 5),
+            Text(name,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400)),
+          ],
+        ),
       ),
     );
   }
