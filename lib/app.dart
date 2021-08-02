965435bdd62dc8ca42ba953c7adcca1a4eea2cff
henri2h <henri.carnot@hotmail.fr>
2021-08-03 01:58:57 +0200

    feat : added dark theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,14 @@
-import 'package:famedlysdk/famedlysdk.dart';
+import 'package:matrix/matrix.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:minestrix/components/minesTrix/MinesTrixTitle.dart';
+import 'package:minestrix/global/Managers/ThemeManager.dart';
 import 'package:minestrix/global/smatrix.dart';
 import 'package:minestrix/screens/createMinesTrixAccount.dart';
 import 'package:minestrix/screens/home/screen.dart';
 import 'package:minestrix/screens/login.dart';
 import 'package:minestrix/global/smatrixWidget.dart';
+import 'package:provider/provider.dart';
 
 class Minestrix extends StatefulWidget {
   @override
@@ -18,51 +20,57 @@ class _MinestrixState extends State<Minestrix> {
   Widget build(BuildContext context) {
     return Matrix(
       child: Builder(
-        builder: (context) => MaterialApp(
-          title: 'MinesTrix client',
-          debugShowCheckedModeBanner: false,
-          home: StreamBuilder<LoginState>(
-            stream: Matrix.of(context).sclient.onLoginStateChanged.stream,
-            builder: (BuildContext context, snapshot) {
-              if (snapshot.hasError) {
-                return Center(child: Text(snapshot.error.toString()));
-              }
-              if (!snapshot.hasData) {
-                return Scaffold(
-                  body: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        H1Title("MINESTRIX"),
-                        H2Title("Loading...."),
-                        CircularProgressIndicator(),
-                      ],
-                    ),
-                  ),
-                );
-              }
-              if (snapshot.data == LoginState.logged) {
-                return StreamBuilder<String>(
-                    stream: Matrix.of(context).sclient.onSRoomsUpdate.stream,
-                    builder: (BuildContext context, snapshot) {
-                      print("Minestrix : room update building home");
-                      SClient sclient = Matrix.of(context).sclient;
+        builder: (context) => Consumer<ThemeNotifier>(
+          builder: (context, theme, _) => MaterialApp(
+            title: 'MinesTrix client',
+            debugShowCheckedModeBanner: false,
 
-                      if (!sclient.sroomsLoaded) {
-                        return Scaffold(
-                          body: Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                        );
-                      } else if (sclient.userRoom == null) {
-                        return MinesTrixAccountCreation();
-                      } else {
-                        return HomeScreen();
-                      }
-                    });
-              }
-              return LoginScreen();
-            },
+            // theme :
+            theme: theme.getTheme(),
+
+            home: StreamBuilder<LoginState>(
+              stream: Matrix.of(context).sclient.onLoginStateChanged.stream,
+              builder: (BuildContext context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+                if (!snapshot.hasData) {
+                  return Scaffold(
+                    body: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          H1Title("MINESTRIX"),
+                          H2Title("Loading...."),
+                          CircularProgressIndicator(),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (snapshot.data == LoginState.logged) {
+                  return StreamBuilder<String>(
+                      stream: Matrix.of(context).sclient.onSRoomsUpdate.stream,
+                      builder: (BuildContext context, snapshot) {
+                        print("Minestrix : room update building home");
+                        SClient sclient = Matrix.of(context).sclient;
+
+                        if (!sclient.sroomsLoaded) {
+                          return Scaffold(
+                            body: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        } else if (sclient.userRoom == null) {
+                          return MinesTrixAccountCreation();
+                        } else {
+                          return HomeScreen();
+                        }
+                      });
+                }
+                return LoginScreen();
+              },
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:minestrix/app.dart';
+import 'package:minestrix/global/Managers/ThemeManager.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   Logger.root.level = Level.ALL;
-  runApp(Minestrix());
+  runApp(ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => new ThemeNotifier(), child: Minestrix()));
 }
