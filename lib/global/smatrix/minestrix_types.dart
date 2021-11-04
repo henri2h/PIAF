82161e8552e9ffcb52d94ddc1dbd40d662597df1
henri2h <henri.carnot@hotmail.fr>
2021-11-04 02:27:10 +0100

    fix : using post + formatting +

--- lib/global/smatrix/minestrix_types.dart
+++ lib/global/smatrix/minestrix_types.dart
@@ -1,4 +1,4 @@
-abstract class MinestrixTypes{
+abstract class MinestrixTypes {
   static const String account = "fr.henri2h.minestrix.account";
   static const String group = "fr.henri2h.minestrix.group";
 }
