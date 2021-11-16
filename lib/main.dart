05c6bff43051aba79c86d8c3316b4434d3bc78e7
henri2h <henri.carnot@hotmail.fr>
2021-11-16 23:30:43 +0100

    fix: removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,6 @@ import 'package:minestrix/app.dart';
 import 'package:minestrix/utils/Managers/ThemeManager.dart';
 import 'package:provider/provider.dart';
 
-/// Load database
-Future<bool> loadHive() async {
-  // if (PlatformInfos.isBetaDesktop) {
-  //   Hive.init((await getApplicationSupportDirectory()).path);
-  // } else {
-  //   await Hive.initFlutter();
-  // }
-  return true;
-}
-
 void main() {
   Logger.root.level = Level.ALL;
   runApp(ChangeNotifierProvider<ThemeNotifier>(
