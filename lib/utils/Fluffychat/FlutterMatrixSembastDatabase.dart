4b58341278b88f1d6c1f7b03620e9ee579081361
henri2h <henri.carnot@hotmail.fr>
2021-11-16 23:05:24 +0100

    fix: dead code

--- lib/utils/Fluffychat/FlutterMatrixSembastDatabase.dart
+++ lib/utils/Fluffychat/FlutterMatrixSembastDatabase.dart
@@ -153,7 +153,6 @@ class _EncryptEncoder extends Converter<Map<String, dynamic>, String> {
         break;
       default:
         throw FormatException('invalid $signature');
-        break;
     }
     return encoded;
   }
