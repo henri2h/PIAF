01e6bcf3e894e69182d0f4ddd51ff5d884b21164
henri2h <henri.carnot@hotmail.fr>
2021-11-21 21:30:19 +0100

    feat: allow to request history while loading user private feed

--- lib/pages/minestrix/user/userFeedPage.dart
+++ lib/pages/minestrix/user/userFeedPage.dart
@@ -29,6 +29,7 @@ class UserFeedPage extends StatefulWidget {
 
 class _UserFeedPageState extends State<UserFeedPage> {
   bool isUserPage = false;
+  bool requestingHistory = false;
 
   Widget buildPage(
       MinestrixClient sclient, MinestrixRoom sroom, Iterable<Event> sevents) {
@@ -135,6 +136,39 @@ class _UserFeedPageState extends State<UserFeedPage> {
                                     const EdgeInsets.symmetric(vertical: 5),
                                 child: Post(event: e),
                               ),
+                            if (sevents.length == 0 ||
+                                sevents.last.type != EventTypes.RoomCreate)
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: MaterialButton(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        children: [
+                                          if (requestingHistory)
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  right: 10),
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            ),
+                                          Text("Load more posts"),
+                                        ],
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      if (requestingHistory == false) {
+                                        setState(() {
+                                          requestingHistory = true;
+                                        });
+                                        await sroom.room
+                                            .requestHistory(historyCount: 50);
+                                        setState(() {
+                                          requestingHistory = false;
+                                        });
+                                      }
+                                    }),
+                              ),
                           ],
                         ),
                       ),
@@ -194,8 +228,7 @@ class _UserFeedPageState extends State<UserFeedPage> {
             Profile p = snapshot.data;
             p.userId = widget.userId!; // fix a nasty bug :(
 
-            return Container(
-                child: ListView(children: [
+            return ListView(children: [
               Container(
                 // alignment: Alignment.bottomCenter,
                 padding: const EdgeInsets.only(left: 40, right: 40, top: 200),
@@ -283,7 +316,7 @@ class _UserFeedPageState extends State<UserFeedPage> {
                   ],
                 ),
               ),
-            ]));
+            ]);
           });
     }
   }
