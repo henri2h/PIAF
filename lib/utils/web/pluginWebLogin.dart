fd45a64c357ed20cb96b43be145ededdbfc407c7
henri2h <henri.carnot@hotmail.fr>
2021-12-05 18:10:22 +0100

    fix: formating

--- lib/utils/web/pluginWebLogin.dart
+++ lib/utils/web/pluginWebLogin.dart
@@ -1,13 +1,12 @@
 import 'dart:html';
 
 class WebLogin {
-
   static String? getToken() {
     String wurl = window.location.href;
 
     Uri u = Uri.parse(wurl);
     String? matrixToken = u.queryParameters["loginToken"];
-  return matrixToken;
+    return matrixToken;
   }
 
   static void launchSame(String url) {
