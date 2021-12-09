6d3a7b1d3bcfab0efc4fc77c48b4bc7c2e87850d
henri2h <henri.carnot@hotmail.fr>
2021-12-09 20:52:43 +0100

    feat: working on addding profile as room

--- lib/pages/account/accountsDetailsPage.dart
+++ lib/pages/account/accountsDetailsPage.dart
@@ -2,11 +2,14 @@ import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
 import 'package:matrix/src/utils/uri_extension.dart';
+import 'package:matrix/src/utils/space_child.dart';
 import 'package:minestrix/components/minesTrix/MinesTrixTitle.dart';
+import 'package:minestrix/partials/components/customFutureButton.dart';
 import 'package:minestrix/router.gr.dart';
 import 'package:minestrix/utils/matrixWidget.dart';
 import 'package:minestrix/utils/minestrix/minestrixClient.dart';
 import 'package:minestrix/utils/minestrix/minestrixRoom.dart';
+import 'package:minestrix_chat/utils/room_profile.dart';
 
 class AccountsDetailsPage extends StatefulWidget {
   const AccountsDetailsPage({Key? key}) : super(key: key);
@@ -20,12 +23,41 @@ class _AccountsDetailsPageState extends State<AccountsDetailsPage> {
   Widget build(BuildContext context) {
     MinestrixClient sclient = Matrix.of(context).sclient!;
 
+    RoomProfile? profile = RoomProfile.getProfileRoom(sclient);
+
     return ListView(
       children: [
         H1Title("Accounts"),
+        if (profile == null)
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: CustomFutureButton(
+                onPressed: () async {
+                  await RoomProfile.createProfileRoom(sclient);
+                  setState(() {});
+                },
+                text: "Create profile",
+                icon: Icon(Icons.create)),
+          ),
+        if (profile != null)
+          for (SpaceChild s in profile.spaceChildren)
+            Builder(builder: (context) {
+              if (s.roomId == false) return Icon(Icons.error);
+
+              return Builder(builder: (context) {
+                Room? r = sclient.getRoomById(s.roomId!);
+                if (r == null) return Icon(Icons.error);
+
+                return RoomProfileListTile(r);
+              });
+            }),
         for (MinestrixRoom sroom in sclient.srooms.values.where((sroom) =>
             sroom.user.id == sclient.userID &&
-            sroom.roomType == SRoomType.UserRoom))
+            sroom.roomType == SRoomType.UserRoom &&
+            (profile == null ||
+                profile.spaceChildren.indexWhere(
+                        (SpaceChild sc) => sc.roomId == sroom.room.id) ==
+                    -1)))
           ListTile(
               leading: CircleAvatar(
                 backgroundImage: sroom.user.avatarUrl == null
@@ -94,6 +126,19 @@ class _AccountsDetailsPageState extends State<AccountsDetailsPage> {
                         Text("Not encrypted")
                       ],
                     ),
+                  if (profile != null &&
+                      profile.spaceChildren.contains(
+                              (SpaceChild sc) => sc.roomId == sroom.room.id) ==
+                          false)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomFutureButton(
+                          onPressed: () async {
+                            await profile.setSpaceChild(sroom.room.id);
+                          },
+                          text: "Add to profile list",
+                          icon: Icon(Icons.add)),
+                    )
                 ],
               ),
               trailing: PopupMenuButton<String>(
@@ -135,5 +180,96 @@ class _AccountsDetailsPageState extends State<AccountsDetailsPage> {
         ),
       ],
     );
+  }
+}
+
+class RoomProfileListTile extends StatelessWidget {
+  const RoomProfileListTile(this.r, {Key? key}) : super(key: key);
+  final Room r;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text((r.name), style: TextStyle(fontWeight: FontWeight.bold))
+            ]),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (r.topic != "")
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: Text(r.topic),
+              ),
+            if (r.joinRules == JoinRules.invite)
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 10),
+                  Text("Private"),
+                ],
+              ),
+            if (r.joinRules == JoinRules.public)
+              Row(
+                children: [
+                  Icon(Icons.public),
+                  SizedBox(width: 10),
+                  Text("Public"),
+                ],
+              ),
+            Row(
+              children: [
+                Icon(Icons.people),
+                SizedBox(width: 10),
+                Text(r.summary.mJoinedMemberCount.toString() + " followers"),
+              ],
+            ),
+            if (r.encrypted)
+              Row(
+                children: [
+                  Icon(Icons.verified_user),
+                  SizedBox(width: 10),
+                  Text("Encrypted")
+                ],
+              ),
+            if (!r.encrypted)
+              Row(
+                children: [
+                  Icon(Icons.no_encryption),
+                  SizedBox(width: 10),
+                  Text("Not encrypted")
+                ],
+              ),
+          ],
+        ),
+        trailing: PopupMenuButton<String>(
+            itemBuilder: (_) => [
+                  PopupMenuItem(
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete_forever, color: Colors.red),
+                          SizedBox(width: 10),
+                          Text("Leave", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                      value: "leave")
+                ],
+            icon: Icon(Icons.more_horiz),
+            onSelected: (String action) async {
+              switch (action) {
+                case "leave":
+                  await r.leave();
+                  /*setState(() {
+                                sclient.srooms.remove(r);
+                              }*/
+                  break;
+                default:
+              }
+            }),
+        onTap: () {
+          // context.pushRoute(UserFeedRoute(sroom: r));
+        });
   }
 }
---
+++ lib/partials/components/customFutureButton.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CustomFutureButton extends StatefulWidget {
+  const CustomFutureButton(
+      {Key? key,
+      required this.onPressed,
+      required this.text,
+      required this.icon})
+      : super(key: key);
+  final AsyncCallback onPressed;
+  final Widget icon;
+  final String text;
+
+  @override
+  _CustomFutureButtonState createState() => _CustomFutureButtonState();
+}
+
+class _CustomFutureButtonState extends State<CustomFutureButton> {
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: loading
+                    ? CircularProgressIndicator(color: Colors.white)
+                    : widget.icon,
+              ),
+              Text(widget.text),
+            ],
+          ),
+        ),
+        onPressed: () async {
+          if (loading) return;
+
+          setState(() {
+            loading = true;
+          });
+          try {
+            await widget.onPressed();
+          } finally {
+            setState(() {
+              loading = false;
+            });
+          }
+        });
+  }
+}
