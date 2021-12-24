883fd82e472be0c96ba99cc711948f8e86672612
henri2h <henri.carnot@hotmail.fr>
2021-12-24 01:42:46 +0100

    feat: request participants in user profile if needed

--- lib/partials/users/userFriendsCard.dart
+++ lib/partials/users/userFriendsCard.dart
@@ -10,27 +10,36 @@ class UserFriendsCard extends StatelessWidget {
   const UserFriendsCard({Key? key, required this.sroom}) : super(key: key);
 
   final MinestrixRoom sroom;
+  Future<List<User>> getUsers() async {
+    return sroom.room.participantListComplete
+        ? sroom.room.getParticipants()
+        : await sroom.room.requestParticipants();
+  }
 
   @override
   Widget build(BuildContext context) {
     MinestrixClient? sclient = Matrix.of(context).sclient;
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: H2Title("Friends"),
-        ),
-        Wrap(alignment: WrapAlignment.spaceBetween, children: [
-          for (User user in sroom.room
-              .getParticipants()
-              .where((User u) =>
-                  u.membership == Membership.join &&
-                  u.id != sclient!.userID &&
-                  u.id != sroom.user.id)
-              .take(9))
-            AccountCard(user: user),
-        ]),
-      ],
-    );
+    return FutureBuilder<List<User>>(
+        future: getUsers(),
+        initialData: sroom.room.getParticipants(),
+        builder: (context, snap) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: H2Title("Friends"),
+              ),
+              Wrap(alignment: WrapAlignment.spaceBetween, children: [
+                for (User user in snap.data!
+                    .where((User u) =>
+                        u.membership == Membership.join &&
+                        u.id != sclient!.userID &&
+                        u.id != sroom.user.id)
+                    .take(9))
+                  AccountCard(user: user),
+              ]),
+            ],
+          );
+        });
   }
 }
