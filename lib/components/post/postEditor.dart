5c8a965f13a4144659d0688f75c705423ac10f75
henri2h <henri.carnot@hotmail.fr>
2021-12-30 19:20:41 +0100

    feat: enhanced post editor

--- lib/components/post/postEditor.dart
+++ lib/components/post/postEditor.dart
@@ -19,8 +19,10 @@ class PostEditorPage extends StatefulWidget {
 
 class _PostEditorPageState extends State<PostEditorPage>
     with SingleTickerProviderStateMixin {
-  String postContent = "";
   FilePickerCross? file;
+
+  TextEditingController _t = TextEditingController();
+  bool _sending = false;
 
   MinestrixRoom? sroom;
 
@@ -50,101 +52,189 @@ class _PostEditorPageState extends State<PostEditorPage>
     sroom = widget.sroom;
     if (sroom == null) sroom = sclient.userRoom;
 
-    return Container(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  MatrixUserImage(
-                      client: sclient,
-                      url: sclient.userRoom!.user.avatarUrl,
-                      width: 48,
-                      thumnail: true,
-                      height: 48),
-                  SizedBox(width: 5),
-                  H1Title("What's up ?"),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Post on " + sroom!.name),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                minLines: 3,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Post content"),
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                onChanged: (String text) {
-                  setState(() {
-                    postContent = text;
-                  });
-                },
-              ),
-            ),
-            Row(
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                IconButton(
-                    icon: Icon(Icons.add_a_photo),
-                    onPressed: () async {
-                      FilePickerCross f =
-                          await FilePickerCross.importFromStorage(
-                              type: FileTypeCross.image);
-                      setState(() {
-                        file = f;
-                      });
-                    }),
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: MinesTrixButton(
-                        icon: Icons.edit,
-                        label: "Send post",
-                        onPressed: () {
-                          sendPost(sclient, postContent);
-                          Navigator.of(context).pop();
-                        }),
-                  ),
+                Row(
+                  children: [
+                    MatrixUserImage(
+                        client: sclient,
+                        url: sclient.userRoom!.user.avatarUrl,
+                        defaultText: sclient.userRoom!.user.displayName,
+                        backgroundColor: Colors.blue,
+                        width: 48,
+                        thumnail: true,
+                        height: 48),
+                    SizedBox(width: 5),
+                    H1Title("What's up ?"),
+                  ],
                 ),
+                Card(
+                  color: _t.text.isEmpty ? null : Colors.green,
+                  child: IconButton(
+                      icon: _sending
+                          ? Center(
+                              child: CircularProgressIndicator(
+                                  color: Colors.white))
+                          : Icon(Icons.send, color: Colors.white),
+                      onPressed: _sending || _t.text.isEmpty
+                          ? null
+                          : () async {
+                              setState(() {
+                                _sending = true;
+                              });
+
+                              await sendPost(sclient, _t.text);
+
+                              setState(() {
+                                _sending = false;
+                              });
+                              Navigator.of(context).pop();
+                            }),
+                )
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Post preview", style: TextStyle(fontSize: 20)),
-            ),
-            if (file != null) Image.memory(file!.toUint8List()),
-            if (file != null)
-              IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    setState(() {
-                      file = null;
-                    });
-                  }),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MarkdownBody(
-                  data: postContent,
-                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context))
-                      .copyWith(
-                          blockquotePadding: const EdgeInsets.only(left: 14),
-                          blockquoteDecoration: const BoxDecoration(
-                              border: Border(
-                                  left: BorderSide(
-                                      color: Colors.white70, width: 4)))),
-                  onTapLink: (text, href, title) async {
-                    if (href != null) {
-                      await _launchURL(href);
-                    }
-                  }),
-            )
-          ],
-        ));
+          ),
+          Expanded(
+            child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: TabBarView(
+                  children: [
+                    ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              MatrixUserImage(
+                                  client: sclient,
+                                  url: sroom!.room.avatar,
+                                  defaultText: sroom!.room.topic,
+                                  backgroundColor: Colors.blue,
+                                  width: 42,
+                                  thumnail: true,
+                                  height: 42),
+                              SizedBox(width: 8),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Post on " + sroom!.name,
+                                        style: TextStyle(fontSize: 22)),
+                                    SizedBox(height: 4),
+                                    Text(sroom!.room.topic)
+                                  ],
+                                ),
+                              ),
+                              Card(
+                                child: IconButton(
+                                    icon: Icon(Icons.add_a_photo),
+                                    onPressed: () async {
+                                      FilePickerCross f = await FilePickerCross
+                                          .importFromStorage(
+                                              type: FileTypeCross.image);
+                                      setState(() {
+                                        file = f;
+                                      });
+                                    }),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 6),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                              minLines: 3,
+                              controller: _t,
+                              cursorColor: Colors.grey,
+                              //controller: _searchController,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 12),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                prefixIcon:
+                                    Icon(Icons.edit, color: Colors.grey),
+                                filled: true,
+                                hintStyle: TextStyle(color: Colors.grey),
+                                labelText: "Post content",
+                                labelStyle: TextStyle(color: Colors.grey),
+                                alignLabelWithHint: true,
+                                hintText: "Post content",
+                              ),
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              onChanged: (_) => setState(() {})),
+                        ),
+                      ],
+                    ),
+                    ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("Post preview",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                        if (file != null) Image.memory(file!.toUint8List()),
+                        if (file != null)
+                          IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  file = null;
+                                });
+                              }),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: MarkdownBody(
+                              data: _t.text,
+                              styleSheet: MarkdownStyleSheet.fromTheme(
+                                      Theme.of(context))
+                                  .copyWith(
+                                      blockquotePadding:
+                                          const EdgeInsets.only(left: 14),
+                                      blockquoteDecoration: const BoxDecoration(
+                                          border: Border(
+                                              left: BorderSide(
+                                                  color: Colors.white70,
+                                                  width: 4)))),
+                              onTapLink: (text, href, title) async {
+                                if (href != null) {
+                                  await _launchURL(href);
+                                }
+                              }),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+          ),
+          const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.edit)),
+              Tab(icon: Icon(Icons.preview)),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
