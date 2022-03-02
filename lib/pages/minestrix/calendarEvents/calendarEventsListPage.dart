33f8c1cb4caa8f1e949acefb0dfeb6edc30aece5
henri2h <henri.carnot@hotmail.fr>
2022-03-02 18:20:19 +0100

    feat: display list of events

--- lib/pages/minestrix/calendarEvents/calendarEventsListPage.dart
+++ lib/pages/minestrix/calendarEvents/calendarEventsListPage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
+import 'package:minestrix/partials/calendarEvents/calendarEventsCard.dart';
 import 'package:minestrix/partials/components/layouts/customHeader.dart';
+import 'package:minestrix/utils/matrixWidget.dart';
+import 'package:minestrix/utils/minestrix/minestrixClient.dart';
+import 'package:minestrix_chat/utils/social/calendar_events/calendar_events_extension.dart';
 
 class CalendarEventListPage extends StatefulWidget {
   const CalendarEventListPage({Key? key}) : super(key: key);
@@ -11,8 +16,15 @@ class CalendarEventListPage extends StatefulWidget {
 class _CalendarEventListPageState extends State<CalendarEventListPage> {
   @override
   Widget build(BuildContext context) {
+    final MinestrixClient sclient = Matrix.of(context).sclient!;
     return ListView(children: [
-      CustomHeader("Events"),
+      CustomHeader("Calendar events"),
+      for (Room room in sclient.calendarEvents)
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CalendarEventCard(room: room),
+        ),
+      if (sclient.calendarEvents.isEmpty) Text("No event found")
     ]);
   }
 }
