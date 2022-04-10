070985269aff37522617b4f635e4b7c40f551217
henri2h <henri.carnot@hotmail.fr>
2022-04-10 02:52:34 +0200

    fix: imports

--- lib/partials/navbar.dart
+++ lib/partials/navbar.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/material.dart';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_route/src/router/auto_router_x.dart';
-import 'package:matrix/matrix.dart';
-import 'package:minestrix_chat/partials/matrix_image_avatar.dart';
-
+import 'package:flutter/material.dart';
 import 'package:minestrix/partials/feed/notficationBell.dart';
 import 'package:minestrix/router.gr.dart';
 import 'package:minestrix/utils/matrixWidget.dart';
-import 'package:minestrix/utils/minestrix/minestrixClient.dart';
 
 class NavBarDesktop extends StatelessWidget {
   const NavBarDesktop({Key? key}) : super(key: key);
