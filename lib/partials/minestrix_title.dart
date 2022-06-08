b397a17eeda243709c7e39bc3e7e56209299d93f
henri2h <henri.carnot@hotmail.fr>
2022-06-09 00:51:58 +0200

    feat: properly added icon on title card

--- lib/partials/minestrix_title.dart
+++ lib/partials/minestrix_title.dart
@@ -8,15 +8,22 @@ class MinestrixTitle extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(50),
       child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Image.asset("assets/icon_512.png", width: 120, height: 120),
-        SizedBox(height: 60),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           children: [
-            Text("MinesTRIX",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w800)),
-            Text("A privacy focused social media based on MATRIX",
-                style: TextStyle(fontSize: 22))
+            Image.asset("assets/icon_512.png", width: 72, height: 72),
+            SizedBox(width: 28),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("MinesTRIX",
+                      style:
+                          TextStyle(fontSize: 34, fontWeight: FontWeight.w800)),
+                  Text("A privacy focused social media based on MATRIX",
+                      style: TextStyle(fontSize: 20))
+                ],
+              ),
+            ),
           ],
         ),
       ]),
