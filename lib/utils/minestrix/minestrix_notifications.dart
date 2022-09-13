ac4f311ee1d3fdcf6fe054b5ba144161583695ea
henri2h <henri.carnot@hotmail.fr>
2022-09-14 00:07:22 +0200

    feat: re wrote sync update

--- lib/utils/minestrix/minestrix_notifications.dart
+++ lib/utils/minestrix/minestrix_notifications.dart
@@ -1,10 +1,27 @@
 import 'dart:async';
 
 import 'package:matrix/matrix.dart';
+import 'package:minestrix_chat/config/matrix_types.dart';
 import 'minestrix_client_extension.dart';
 
 extension MinestrixNotifications on Client {
-  Stream get onMinestrixUpdate => onSync.stream.where((up) => up.hasRoomUpdate);
+  Stream<SyncUpdate> get onMinestrixUpdate =>
+      onSync.stream.where((up) => up.hasRoomUpdate);
+
+  Stream<SyncUpdate> get onNewPost => onSync.stream.where((up) {
+        if (up.rooms?.join?.values != null) {
+          for (var room in up.rooms!.join!.values) {
+            if (room.timeline?.events?.isNotEmpty == true) {
+              for (var event in room.timeline!.events!) {
+                if (event.type == MatrixTypes.post) {
+                  return true;
+                }
+              }
+            }
+          }
+        }
+        return false;
+      });
 
   /// This method should fetch all the user events and build the notification feed
   // TODO : add a way to check if the user has been mentionned or not.
