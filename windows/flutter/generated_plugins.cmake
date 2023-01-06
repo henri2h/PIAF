f3f70665f7cba79ee6999d11698f0bce084c069a
henri2h <henri.carnot@hotmail.fr>
2023-01-06 22:44:34 +0100

    chore: update dep

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -11,6 +11,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_secure_storage_windows
   flutter_webrtc
+  permission_handler_windows
   sentry_flutter
   url_launcher_windows
 )
