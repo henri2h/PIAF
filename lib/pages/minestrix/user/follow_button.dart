b58a8752ef62f75d6c1377c6237597a2381a6a75
henri2h <henri.carnot@hotmail.fr>
2023-03-01 18:42:43 +0100

    feat: enable unfollowing someone

--- lib/pages/minestrix/user/follow_button.dart
+++ lib/pages/minestrix/user/follow_button.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
 import 'package:minestrix_chat/utils/matrix_widget.dart';
@@ -6,12 +7,24 @@ import 'package:minestrix_chat/utils/spaces/space_extension.dart';
 import '../../../partials/components/buttons/custom_future_button.dart';
 
 class FollowingIndicator extends StatelessWidget {
-  const FollowingIndicator({Key? key, required this.room}) : super(key: key);
+  const FollowingIndicator({Key? key, required this.room, this.onUnfollow})
+      : super(key: key);
 
   final RoomWithSpace room;
+  final VoidCallback? onUnfollow;
 
-  Future<void> follow(Client client) async {
-    if (room.space == null) return;
+  Future<void> follow(Client client, BuildContext context) async {
+    if (room.space == null) {
+      final result = await showOkCancelAlertDialog(
+          context: context,
+          title: "Are you sure you want to unfollow this user?",
+          message:
+              "This action cannot be undone. If you were invited to this room you won't be able to join again without a new invitation.");
+      if (result == OkCancelResult.ok) {
+        await room.room?.leave();
+        onUnfollow?.call();
+      }
+    }
     switch (room.space!.joinRule ?? '') {
       case "public":
         await client.joinRoom(room.space!
@@ -36,7 +49,7 @@ class FollowingIndicator extends StatelessWidget {
         padding: const EdgeInsets.all(6),
         icon: Icon(following ? Icons.person : Icons.person_add,
             color: Theme.of(context).colorScheme.onPrimary),
-        onPressed: () async => follow(Matrix.of(context).client),
+        onPressed: () async => follow(Matrix.of(context).client, context),
         children: [
           Text(following ? "Following" : "Follow",
               style: TextStyle(color: Theme.of(context).colorScheme.onPrimary))
