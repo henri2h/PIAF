78a366e17fd9d4ae93fa9937d72761041eef5ca0
henri2h <henri.carnot@hotmail.fr>
2023-10-01 00:50:39 +0200

    fix: update mutual rooms

--- lib/lib/utils/matrix/mutual_rooms_extension.dart
+++ lib/lib/utils/matrix/mutual_rooms_extension.dart
@@ -7,13 +7,15 @@ import 'package:http/http.dart' as http;
 extension MutualRooms on Client {
   Future<List<String>?> getMutualRoomsWithUser(String userID) async {
     final requestUri = Uri(
-        path:
-            '_matrix/client/unstable/uk.half-shot.msc2666/user/mutual_rooms/${Uri.encodeComponent(userID)}');
+        path: '_matrix/client/unstable/uk.half-shot.msc2666/user/mutual_rooms',
+        queryParameters: {"user_id": userID});
     final request = http.Request('GET', baseUri!.resolveUri(requestUri));
     request.headers['authorization'] = 'Bearer ${bearerToken!}';
     final response = await httpClient.send(request);
     final responseBody = await response.stream.toBytes();
-    if (response.statusCode != 200) unexpectedResponse(response, responseBody);
+    if (response.statusCode != 200) {
+      unexpectedResponse(response, responseBody);
+    }
     final responseString = utf8.decode(responseBody);
     Map<String, dynamic> json = jsonDecode(responseString);
 
