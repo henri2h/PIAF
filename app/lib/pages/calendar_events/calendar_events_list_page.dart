014b9e6d44a05f92f6a79cf2b4beaa5362092c4e
henri2h <henri.carnot@hotmail.fr>
2023-10-01 02:22:50 +0200

    fix: events

--- app/lib/pages/calendar_events/calendar_events_list_page.dart
+++ app/lib/pages/calendar_events/calendar_events_list_page.dart
@@ -6,7 +6,6 @@ import 'package:minestrix_chat/utils/matrix_widget.dart';
 
 import '../../partials/calendar_events/calendar_event_card.dart';
 import '../../partials/calendar_events/calendar_event_create_widget.dart';
-import '../../partials/components/layouts/custom_header.dart';
 import '../../partials/components/minestrix/minestrix_title.dart';
 
 @RoutePage()
@@ -58,48 +57,52 @@ class _CalendarEventListPageState extends State<CalendarEventListPage> {
             return event!.start!.compareTo(DateTime.now()) >= 0;
           }).toList();
 
-          return ListView(children: [
-            CustomHeader(
-              title: "Calendar events",
-              actionButton: [
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Events"),
+              actions: [
                 IconButton(
                   icon: const Icon(Icons.add),
                   onPressed: createCalendarEvent,
                 )
               ],
             ),
-            if (post.isNotEmpty) const H2Title("Future events"),
-            if (post.isNotEmpty)
-              GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 470, mainAxisExtent: 290),
-                  itemCount: post.length,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, pos) {
-                    final item = post[pos];
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CalendarEventCard(room: item),
-                    );
-                  }),
-            if (prev.isNotEmpty) const H2Title("Previous events"),
-            if (prev.isNotEmpty)
-              GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 470, mainAxisExtent: 290),
-                  itemCount: prev.length,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, pos) {
-                    final item = prev[pos];
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CalendarEventCard(room: item),
-                    );
-                  }),
-            if (prev.isEmpty && post.isEmpty) const Text("No event found")
-          ]);
+            body: ListView(children: [
+              if (post.isNotEmpty) const H2Title("Future events"),
+              if (post.isNotEmpty)
+                GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 470, mainAxisExtent: 290),
+                    itemCount: post.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, pos) {
+                      final item = post[pos];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CalendarEventCard(room: item),
+                      );
+                    }),
+              if (prev.isNotEmpty) const H2Title("Previous events"),
+              if (prev.isNotEmpty)
+                GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 470, mainAxisExtent: 290),
+                    itemCount: prev.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, pos) {
+                      final item = prev[pos];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CalendarEventCard(room: item),
+                      );
+                    }),
+              if (prev.isEmpty && post.isEmpty) const Text("No event found")
+            ]),
+          );
         });
   }
 }
