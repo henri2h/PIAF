e06b03ec5058a6212b02c64649feeb2700299679
henri2h <henri.carnot@hotmail.fr>
2024-05-21 21:26:56 +0200

    fix: update room list when roomList is loaded

--- lib/chat/partials/chat/room_list/simple_room_list.dart
+++ lib/chat/partials/chat/room_list/simple_room_list.dart
@@ -17,19 +17,24 @@ class SimpleRoomList extends StatelessWidget {
     return Consumer<ChatPageState>(builder: (context, controller, _) {
       final client = controller.client;
 
-      return StreamBuilder<SyncUpdate>(
-          stream: client.onSync.stream.where((up) => up.hasRoomUpdate),
-          builder: (context, snapshot) {
-            final rooms = controller.getFilteredRoomList(client);
+      return FutureBuilder(
+          future: client.roomsLoading,
+          builder: (context, snap) {
+            return StreamBuilder<SyncUpdate>(
+                stream: client.onSync.stream.where((up) => up.hasRoomUpdate),
+                builder: (context, snapshot) {
+                  final rooms = controller.getFilteredRoomList(client);
 
-            return RoomList(
-              controller: controller,
-              scrollController: scrollController,
-              client: client,
-              sortedRooms: rooms,
-              isMobile: true,
-              onAppBarClicked: () => context.navigateTo(const SettingsRoute()),
-            );
+                  return RoomList(
+                    controller: controller,
+                    scrollController: scrollController,
+                    client: client,
+                    sortedRooms: rooms,
+                    isMobile: true,
+                    onAppBarClicked: () =>
+                        context.navigateTo(const SettingsRoute()),
+                  );
+                });
           });
     });
   }
